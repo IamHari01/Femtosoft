184e35b7d63e82a9c67b801d71098860830b3cdc
iamHari01 <gowribalan95@gmail.com>
2025-07-07 12:19:03 +0530

    I create a program for a simple calculator

---
+++ Calculator/script.js
@@ -0,0 +1,42 @@
+//Calculator Program
+
+const display = document.getElementById('display');
+
+function appendTodisplay(input){
+    display.value += input;
+
+}
+function clearDisplay(){
+    display.value = '';
+}
+function deleteOnce(){
+   if(display.value === 'Errorஉ' ){
+    clearDisplay();
+    
+   }else{
+    display.value = display.value.slice(0, -1);
+   }
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Errorஉ";
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+
+    // Allow numbers, operators, dot, and Enter
+    if (/[\d+\-*/.]/.test(key)) {
+      display.value += key;
+    } else if (key === 'Enter') {
+      calculate();
+    } else if (key === 'Backspace') {
+        deleteOnce();
+    } else if (key === 'Escape') {
+      display.value = '';
+    }
+  });
